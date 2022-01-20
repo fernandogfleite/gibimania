238391c4f6ee1916f70c309d28beee8c90630947
Fernando Leite <fernando.leite@wlcsolucoes.com.br>
2022-01-20 20:26:53 -0300

    Ajustes para dar deploy no heroku

--- gibimania/settings.py
+++ gibimania/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lot67@&96db=pmmk28n8zoh+23u!zh--u_j82@ac!)dnhdb^z+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
